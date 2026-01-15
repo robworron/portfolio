3b120af1362f8618cd7e0451e50d50c5e3047af5
rworron <rworron94@gmail.com>
2026-01-15 17:19:49 -0500

    added sitemanifest and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+export const metadata = {
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
+};
+
 const geistSans = Geist({
   subsets: ["latin"],
 });
